9e1044f7d33b0bd3c156b30e389d148b519c263c
stuart.warren <stuart.warren@ocado.com>
2022-08-16 10:18:36 +0100

    remove exwm

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -49,7 +49,7 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 
-(package! exwm)
+;; (package! exwm)
 (package! evil-tutor)
 (package! tree-sitter)
 (package! tree-sitter-langs)
